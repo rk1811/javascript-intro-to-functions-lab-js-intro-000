7327c7b2196d56764f345b5071120b7f363b0084
passionate-array-0100 <robertkahn92@gmail.com>
2020-06-17 19:40:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function sayHiToGrandma(string){
   if (string===string.toLowerCase()){
     return 'I can\'t hear you!'}
   else if (string===string.toUpperCase()) {
-    return "YES INDEED!"
-  }}
+    return "YES INDEED!"}
+    else return "I love you, too"
